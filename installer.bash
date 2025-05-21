c52d002ace411fbc7ad09e7b192d6b3a3e00ac29
Luo25177 <beloved25177@126.com>
2025-05-21 16:16:00 +0800

    2025.5.21

---
+++ installer.bash
@@ -0,0 +1,18 @@
+pyinstaller \
+  --onefile \
+  --windowed \
+  --name "MyApp" \
+  --icon "img/logo.png" \
+  --add-data "img/logo.png;data" \
+  --add-data "fonts/MeriendaOne-Regular.ttf;data" \
+  --add-data "utils/delete_api.py;utils" \
+  --add-data "utils/drop_image_widget.py;utils" \
+  --add-data "utils/message_box.py;utils" \
+  --add-data "utils/my_enum.py;utils" \
+  --add-data "utils/show_img_widget.py;utils" \
+  --add-data "utils/sidebar.py;utils" \
+  --add-data "utils/stack_page.py;utils" \
+  --add-data "utils/switch_button.py;utils" \
+  --add-data "utils/upload_api.py;utils" \
+  --add-data "utils/work_thread.py;utils" \
+  gitpic.py
